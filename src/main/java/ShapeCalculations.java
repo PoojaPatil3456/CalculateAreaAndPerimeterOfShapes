337706c838e8d086de243c21e58168370f612249
PoojaPatil3456 <poojapatil2402@gmail.com>
2021-08-18 22:31:37 +0530

    Requirement 1: Area of rectangle calculated

--- src/main/java/ShapeCalculations.java
+++ src/main/java/ShapeCalculations.java
@@ -1,2 +1,11 @@
 public class ShapeCalculations {
+    int length;
+    int width;
+    public ShapeCalculations(int length,int width){
+        this.length=length;
+        this.width=width;
+    }
+    public int area() {
+        return length*width;
+    }
 }
--- src/test/java/ShapeTestCases.java
+++ src/test/java/ShapeTestCases.java
@@ -1,2 +1,35 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ShapeTestCases {
+    @Nested
+    public class RectangleAreaTestCases {
+        @Test
+        void toCalculateAreaOfRectangleForPositiveLengthAndWidth() {
+            ShapeCalculations shape = new ShapeCalculations(10, 5);
+            assertEquals(50, shape.area());
+        }
+
+        @Test
+        void toCalculateAreaOfRectangleForNegativeLengthAndWidth() {
+            ShapeCalculations shape = new ShapeCalculations(-10, -5);
+            assertEquals(50, shape.area());
+        }
+
+        @Test
+        void toCalculateAreaOfRectangleForNegativeLengthAndPositiveWidth() {
+            ShapeCalculations shape = new ShapeCalculations(-10, 5);
+            assertEquals(-50, shape.area());
+        }
+
+        @Test
+        void toCalculateAreaOfRectangleForPositiveLengthAndNegativeWidth() {
+            ShapeCalculations shape = new ShapeCalculations(10, -5);
+            assertEquals(-50, shape.area());
+        }
+    }
+
 }
